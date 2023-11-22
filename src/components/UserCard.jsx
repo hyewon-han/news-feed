530ed79ca07049b3d8929d4c5c77776a0b334e88
pompomko <sdw12334@gmail.com>
2023-11-22 20:28:38 +0900

    이페이지 작성

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import defaultUser from 'assets/defaultUser.png';
+import theme from 'styles/Theme';
+
+export default function UserCard({ User }) {
+  return (
+    <LetterWrapper>
+      <UserInfo>
+        <AvataFigure>
+          <img src={User.avatar ?? defaultUser} alt="아바타이미지" /> //없으면 디폴트 유저사진
+        </AvataFigure>
+        <NicknameAndData>
+          <p>이름 :{User.nickname}</p>
+          <p>메일주소 :{User.email}</p>
+          <p>MBTI :{User.mbti}</p>
+        </NicknameAndData>
+      </UserInfo>
+      <Content>{User.Content}Edit profile</Content>
+    </LetterWrapper>
+  );
+}
+
+const LetterWrapper = styled.li`
+  display: flex;
+  gap: 12px;
+  flex-direction: column;
+  color: ${theme.color.blue};
+  padding: 12px;
+
+  border-radius: 12px;
+`;
+const UserInfo = styled.div`
+  display: flex;
+  gap: 12px;
+  flex-direction: center;
+`;
+
+const AvataFigure = styled.figure`
+  //figure태그안에는 이미 자식요소로 이미지 태그가있음
+  width: 100px;
+  height: 100px;
+  border-radius: 50%; //높이넓이가같을때 보더 래디우스를 50%주면 동그래짐
+  overflow: hidden; //이미지가 삐져나오면 숨겨줌
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; //이미지 크기가 피규어 크기만큼 꽉차게 빈틈없이만들어줌
+    border-radius: 50%;
+  }
+`;
+const NicknameAndData = styled.div`
+  display: flex;
+  gap: 6px;
+  flex-direction: column;
+`;
+
+const Content = styled.button`
+  border-radius: 12px;
+  background-color: ${theme.color.pink};
+  padding: 12px;
+  margin-left: 390px;
+  white-space: nowrap;
+`;
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import theme from 'styles/Theme';
+import UserInfo from 'components/UserInfo';
 
 function MyProfile() {
-  return <div>MyProfile</div>;
+  return (
+    <ProfileBox>
+      게시글 박스
+      <UserInfo>유저 정보</UserInfo>
+    </ProfileBox>
+  );
 }
 
 export default MyProfile;
+
+const ProfileBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${theme.color.green};
+  border: 1px solid ${theme.color.pink};
+  margin: 20px auto 10px auto;
+  width: 70%;
+  height: 85%;
+  min-width: 580px;
+`;
+
+const WriteBox = styled.ul`
+  border: 1px solid ${theme.color.blue};
+  width: 90%;
+  height: 65%;
+`;
+
+const WriteList = styled.li``;
