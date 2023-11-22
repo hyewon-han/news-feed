c43d768ccb4418bb248093dd7c5d8c9f4977c6bb
hyewon-han <hw1635@hanmail.net>
2023-11-22 10:45:49 +0900

    upload page 생성

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -5,6 +5,7 @@ import Detail from 'pages/Detail';
 import Join from 'pages/Join';
 import Login from 'pages/Login';
 import MyProfile from 'pages/MyProfile';
+import Upload from 'pages/Upload';
 
 const Router = () => {
   return (
@@ -16,6 +17,7 @@ const Router = () => {
           <Route path="/users/:id" element={<MyProfile />} />
           <Route path="/join" element={<Join />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/upload" element={<Upload />} />
         </Routes>
       </Layout>
     </BrowserRouter>
