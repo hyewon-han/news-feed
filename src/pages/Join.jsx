fb97fa3a6220fe1b72c9a2afbcd96c80c630778f
Seth Jeon <sethchun@gmail.com>
2023-11-23 19:15:03 +0900

    [UPDATE] 회원가입시 이메일 비밀번호 validation check

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,6 +1,6 @@
+import React, { useRef, useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from 'firebase.js';
-import React, { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createUser } from 'redux/modules/user';
 import { useNavigate } from 'react-router-dom';
@@ -8,32 +8,41 @@ import { useNavigate } from 'react-router-dom';
 function Join() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState(''); // Added confirmPassword state
   const [name, setName] = useState('');
-  //const [userData, setUserData] = useState([]);
+  const [passwordError, setPasswordError] = useState(''); // Added passwordError state
+  const [emailError, setEmailError] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const selectRef = useRef();
 
   const onChange = (event) => {
-    const {
-      target: { name, value }
-    } = event;
+    const { name, value } = event.target;
     if (name === 'email') {
       setEmail(value);
-    }
-    if (name === 'password') {
+    } else if (name === 'password') {
       setPassword(value);
-    }
-    if (name === 'name') {
+    } else if (name === 'confirmPassword') {
+      setConfirmPassword(value);
+    } else if (name === 'name') {
       setName(value);
     }
   };
 
   const selectMbti = () => {
-    const selectedMbti = selectRef.current.value;
-    return selectedMbti;
+    return selectRef.current.value;
   };
+
+  const validatePassword = () => {
+    if (password !== confirmPassword) {
+      setPasswordError('비밀번호가 일치하지 않습니다.');
+      return false;
+    }
+    setPasswordError('');
+    return true;
+  };
+
   const createUserObj = (userId) => {
     const userObj = {
       name,
@@ -42,15 +51,34 @@ function Join() {
       userId,
       mbti: selectMbti()
     };
-    //setUserData((prev) => [userObj, ...prev]);
     dispatch(createUser(userObj));
     setName('');
     setPassword('');
+    setConfirmPassword('');
     setEmail('');
+  };
+
+  const validateEmail = (email) => {
+    // Simple email validation using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
 
   const signUp = async (event) => {
     event.preventDefault();
+
+    // Validate email
+    if (!validateEmail(email)) {
+      setEmailError('유효한 이메일 주소를 입력하세요.');
+      return;
+    } else {
+      setEmailError('');
+    }
+
+    // Validate password
+    if (!validatePassword()) {
+      return;
+    }
 
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -64,24 +92,34 @@ function Join() {
       console.log('error with signUp', errorCode, errorMessage);
     }
   };
+
   return (
     <div>
       <form>
         <div>
-          <label>이메일 : </label>
-          <input type="email" value={email} name="email" onChange={onChange} required></input>
+          <h1>이메일 </h1>
+          <input type="email" value={email} name="email" onChange={onChange} required />
+          {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
         </div>
         <div>
-          <label>비밀번호 : </label>
-          <input type="password" value={password} name="password" onChange={onChange} required></input>
+          <h1>비밀번호 </h1>
+          <input type="password" value={password} name="password" onChange={onChange} required />
         </div>
         <div>
-          <label>이름 : </label>
-          <input type="text" value={name} name="name" onChange={onChange} required></input>
+          <h1>비밀번호 재확인</h1>
+          <input type="password" value={confirmPassword} name="confirmPassword" onChange={onChange} required />
+          {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
         </div>
         <div>
-          <label htmlFor="mbti">MBTI</label>
+          <h1>이름 </h1>
+          <input type="text" value={name} name="name" onChange={onChange} required />
+        </div>
+        <div>
+          <h1 htmlFor="mbti">MBTI</h1>
           <select id="mbti" onChange={selectMbti} ref={selectRef}>
+            <option value="" disabled hidden>
+              MBTI
+            </option>
             <option value="intj">INTJ</option>
             <option value="intp">INTP</option>
             <option value="entj">ENTJ</option>
